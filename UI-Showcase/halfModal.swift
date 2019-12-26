0cdb6a2b1ccb7758445ccf0d535854bca08f5930
chenbao <chenbaocn@icloud.com>
2019-12-26 18:06:42 +0800

    给 halfModal 添加了两个 圆角

--- UI-Showcase/halfModal.swift
+++ UI-Showcase/halfModal.swift
@@ -10,14 +10,6 @@ import Combine
 import nav
 import SwiftUI
 
-struct searchBar_Previews: PreviewProvider {
-    static var previews: some View {
-        halfModal()
-    }
-}
-
-/// 决定用这个来模仿地图里面的那个 东西
-/// 果然, 越花哨的东西, 写起来越麻烦😁
 struct halfModal: View {
     var body: some View {
         ScrollView {
@@ -31,6 +23,8 @@ struct halfModal: View {
     }
 }
 
+/// 决定用这个来模仿地图里面的那个 东西
+/// 果然, 越花哨的东西, 写起来越麻烦😁
 struct halfModalAndGesture: ViewModifier {
     func body(content: Content) -> some View {
         GeometryReader { (g: GeometryProxy) in
@@ -44,38 +38,36 @@ struct halfModalAndGesture: ViewModifier {
                 Spacer()
             }
         }
-
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
-        .background(Color.blue)
         .overlay(
             content
                 .offset(x: 0, y: 100)
                 .disabled(self.currnetHalfState.halfOrTop == .top ? false : true)
         )
-        .offset(y: self.finalOffset)
 
-        .animation(self.gestureOffset.height == 0 ? Animation.spring(response: Double(0.3)) : .none)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+        .background(RoundedCorners(color: .blue, tl: 20, tr: 20))
+
+        .offset(y: self.finalOffset) /// 位置的 移动
+
+        .animation(self.gestureOffset.height == 0 ?
+            .spring(response: Double(0.3)) : .none) /// 只在 拖动结束时的位置 归位 才 使用 动画
 
         .gesture(DragGesture()
             .updating(self.$gestureOffset) { v, s, _ in
 
                 /// 滑到 顶部时, 就不需要在 滑动了
                 if let pill = pillLocation {
-                    if pill > 0 {
+                    if pill > -100 {
                         s = v.translation /// 用于正在拖动时的 移动
                     }
                 }
 
                 if pillLocation == nil {
                     pillLocation = 0
+                } else {
+                    /// predictedEndTranslation 更符合 用户的心理预期
+                    pillLocation! += v.predictedEndTranslation.height
                 }
-
-                /// predictedEndTranslation 更符合 用户的心理预期
-                pillLocation! += v.predictedEndTranslation.height
-
-//                if let location = pillLocation {
-//                    self.fatherViewHeight = 1 - (location / self.storedGemotryReder_Size.height)
-//                }
             }
 
             .onChanged { (_: DragGesture.Value) in
@@ -105,8 +97,13 @@ struct halfModalAndGesture: ViewModifier {
 
     @GestureState private var gestureOffset: CGSize = .zero
 
-    /// 当用户停止滑动时 halfModal 的 偏移量
-    /// 根据
+    @State private var storedGemotryReder_Size: CGSize = .zero
+
+    private var currnetHalfState = stater()
+
+    private var touchBegingState = stater()
+
+    /// 当用户滑动时 halfModal 的 偏移量
     var finalOffset: CGFloat {
         nonmutating get {
             if self.gestureOffset.height != 0 { /// 拖拽中
@@ -123,12 +120,6 @@ struct halfModalAndGesture: ViewModifier {
         }
     }
 
-    @State private var storedGemotryReder_Size: CGSize = .zero
-
-    private var currnetHalfState = sdafadsfasdfdasfasfsdf()
-
-    private var touchBegingState = sdafadsfasdfdasfasfsdf()
-
     private func f(_ c: CGFloat, h: HalfOrTop) -> CGFloat {
         switch h {
         case .top: return 0
@@ -137,15 +128,6 @@ struct halfModalAndGesture: ViewModifier {
 
         case .bottom: return c - 100
         }
-    }
-
-//    @State private var midlePointHeight: CGFloat = 0
-    private var midlePointHeight: CGFloat {
-        if let location = pillLocation {
-            return 1 - (location / self.storedGemotryReder_Size.height)
-        }
-
-        return 0
     }
 }
 
@@ -157,10 +139,10 @@ extension halfModalAndGesture {
         HStack {
             Spacer()
 
-//            LineView(fatherViewHeight: self.$fatherViewHeight)
             LineView(fatherViewHeight: self.storedGemotryReder_Size.height)
                 .frame(width: 100, height: 20)
                 .padding()
+
             Spacer()
         }
     }
@@ -179,7 +161,7 @@ extension halfModalAndGesture {
 }
 
 extension halfModalAndGesture {
-    private class sdafadsfasdfdasfasfsdf {
+    private class stater {
         var isFirst = true
 
         var halfOrTop: HalfOrTop = .half
@@ -210,16 +192,38 @@ extension halfModalAndGesture {
     }
 }
 
-extension UIBezierPath {
-    static func bezierPath(
-        controlPoint1X: Double = 0.680,
-        controlPoint1Y: Double = -0.550,
-        controlPoint2X: Double = 0.265,
-        controlPoint2Y: Double = 1.550
-    ) -> UIBezierPath {
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0.0, y: 0.0))
-        path.addCurve(to: CGPoint(x: 1.0, y: -1.0), controlPoint1: CGPoint(x: controlPoint1X, y: -controlPoint1Y), controlPoint2: CGPoint(x: controlPoint2X, y: -controlPoint2Y))
-        return path
+/// code form here : https://swiftui-lab.com/geometryreader-to-the-rescue/
+struct RoundedCorners: View {
+    var color: Color = .black
+    var tl: CGFloat = 0.0
+    var tr: CGFloat = 0.0
+    var bl: CGFloat = 0.0
+    var br: CGFloat = 0.0
+
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+
+                let w = geometry.size.width
+                let h = geometry.size.height
+
+                // We make sure the redius does not exceed the bounds dimensions
+                let tr = min(min(self.tr, h / 2), w / 2)
+                let tl = min(min(self.tl, h / 2), w / 2)
+                let bl = min(min(self.bl, h / 2), w / 2)
+                let br = min(min(self.br, h / 2), w / 2)
+
+                path.move(to: CGPoint(x: w / 2.0, y: 0))
+                path.addLine(to: CGPoint(x: w - tr, y: 0))
+                path.addArc(center: CGPoint(x: w - tr, y: tr), radius: tr, startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+                path.addLine(to: CGPoint(x: w, y: h - br))
+                path.addArc(center: CGPoint(x: w - br, y: h - br), radius: br, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+                path.addLine(to: CGPoint(x: bl, y: h))
+                path.addArc(center: CGPoint(x: bl, y: h - bl), radius: bl, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+                path.addLine(to: CGPoint(x: 0, y: tl))
+                path.addArc(center: CGPoint(x: tl, y: tl), radius: tl, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+            }
+            .fill(self.color)
+        }
     }
 }
